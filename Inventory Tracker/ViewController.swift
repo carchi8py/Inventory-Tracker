b5211489ea281d6b3f2ec04d79f6e1024b1a73cc
Chris Archibald <carchi8py@gmail.com>
2015-10-01 17:04:46 -0700

    Initial Commit

---
+++ Inventory Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inventory Tracker
+//
+//  Created by Chris Archibald on 10/1/15.
+//  Copyright (c) 2015 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
