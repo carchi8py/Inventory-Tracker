7c3f1b10dd93a492c0eca14d529806e911440244
Chris Archibald <carchi8py@gmail.com>
2015-10-06 17:58:13 -0700

    covered App to swift 2.0

--- Inventory Tracker/ViewController.swift
+++ Inventory Tracker/ViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 //Documents Directory
 func documentsDirectory() -> String {
-    let documentsFolderPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as! String
+    let documentsFolderPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] 
     return documentsFolderPath
 }
 
 // File in Documents Directory
 func fileInDocumentsDirectory(filename: String) -> String {
-    return documentsDirectory().stringByAppendingPathComponent(filename)
+    return documentsDirectory().stringByAppendingString(filename)
 }
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -35,7 +35,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imageView.contentMode = UIViewContentMode.ScaleAspectFit
         
         var item = Item(name: "Cup", cost: 9.99, imageName: "Cup.png")
-        println("item: \(item)")
+        print("item: \(item)")
     }
 
     @IBAction func addButtonPressed(sender: AnyObject) {
@@ -46,10 +46,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func saveToDisk() {
         let itemTitle = itemTextField.text
         
-        if !itemTitle.isEmpty {
+        if !itemTitle!.isEmpty {
             let numberFormatter = NSNumberFormatter()
             numberFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
-            let itemCost = numberFormatter.numberFromString(costTextField.text)
+            let itemCost = numberFormatter.numberFromString(costTextField.text!)
             
             if itemCost != nil {
                 
@@ -58,20 +58,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                     saveImage(image, path: imagePath)
                     if let itemCostFormatted = numberFormatter.stringFromNumber(itemCost!) {
                         var textDataToSave = "\(itemTitle), \(itemCostFormatted), \(imagePath)"
-                        println(textDataToSave)
+                        print(textDataToSave)
                         
                         let textPath = fileInDocumentsDirectory("\(itemTitle).txt")
                         saveText(textDataToSave, path: textPath)
                     }
                     
                 } else {
-                    println("Error missing image")
+                    print("Error missing image")
                 }
             } else {
-                println("Error missing cost or invalid value")
+                print("Error missing cost or invalid value")
             }
         } else {
-            println("Error: missing item title")
+            print("Error: missing item title")
         }
     }
     
@@ -79,52 +79,51 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     //Save text
     func saveText(text: String, path: String) -> Bool {
-        var error: NSError? = nil
-        
-        let status = text.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding, error: &error)
-        if !status {
-            println("Error saving file at path: \(path) with error: \(error?.localizedDescription)")
+        do {
+            try text.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding)
+            return true
+        } catch {
+            print("Error saving file at path: \(path) with error: \(error)")
         }
-        return status
+        return false
     }
     
     //load Text
     func loadTextFromPath(path: String) -> String? {
-        var error: NSError? = nil
-        let text = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: &error)
-        if text == nil {
-            println("Error loading text from path \(path) error: \(error)")
+        var text:String?
+        do {
+            try text = String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+        } catch {
+            print("Error loading text from path \(path) error: \(error)")
         }
-        
         return text
     }
     
     func loadImageFromPath(path: String) -> UIImage? {
         let image = UIImage(contentsOfFile: path)
         if image == nil {
-            println("Missing Image at path: \(path)")
+            print("Missing Image at path: \(path)")
         }
         return image
     }
     
     func saveImage(image: UIImage, path: String) -> Bool {
         let jpgImageData = UIImageJPEGRepresentation(image, 1.0)
-        let result = jpgImageData.writeToFile(path, atomically: true)
+        let result = jpgImageData!.writeToFile(path, atomically: true)
         return result
     }
     
     /****** Image Picker Methods ********/
     
     func handleTapGesture(tapGesture: UITapGestureRecognizer) {
-        println("tap")
+        print("tap")
         
         var imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         presentViewController(imagePicker, animated: true, completion: nil)
         
     }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         
         dismissViewControllerAnimated(true, completion: nil)
         
